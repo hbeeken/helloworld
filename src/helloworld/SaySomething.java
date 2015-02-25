7f8aa000830108cf6d998f993ca30a0bf4bf45de
hbeeken <hbeeken@users.noreply.github.com>
2015-02-25 10:24:46 +0000

    squashing commit

--- src/helloworld/SaySomething.java
+++ src/helloworld/SaySomething.java
@@ -4,6 +4,13 @@ public class SaySomething {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		SaySomething ss = new SaySomething();
+		ss.foo();
+	}
+	
+	public void foo() {
+		System.out.println("foo");
+		System.out.println("bar");
 	}
 
 }
